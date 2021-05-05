3ce8a342b9b6a0a3dbb9e9daaaa8f971db38c4f4
jovid1242 <jovid1242@gmail.com>
2021-05-05 18:12:10 +0500

    fix

--- src/components/modal/Index.js
+++ src/components/modal/Index.js
@@ -40,6 +40,7 @@ export default function Index(props) {
             return
         }
     }
+    // jovid1242jivO
 
     const valideModal = () => {
         const err = validation.modalValidation(form)
