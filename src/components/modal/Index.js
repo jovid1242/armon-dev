720d82dbd9a67da8b5d499453958b669e4cc1286
jovid1242 <jovid1242@gmail.com>
2021-05-05 17:43:25 +0500

    add validation in modal feedback

--- src/components/modal/Index.js
+++ src/components/modal/Index.js
@@ -1,8 +1,55 @@
-import React from 'react'
-import { Modal, Form } from 'react-bootstrap';
+import React, { useState } from 'react'
+import { Modal, Form, InputGroup, Col } from 'react-bootstrap';
+import axios from 'axios'
+import FormData from 'form-data'
+import validation from './validateModal'
+import ReactHtmlParser from 'react-html-parser'
 import './Index.css'
 
 export default function Index(props) {
+    const [validetAlert, setValidetAlert] = useState(null)
+    const [res, setRes] = useState(null)
+    const [form, setForm] = useState({
+        name: null,
+        phone: null
+    })
+
+    const handleModalInput = (e) => {
+        const name = e.target.name
+        const value = e.target.value
+        const data = form
+        data[name] = value
+        setForm(data)
+    }
+
+    const submitForm = (e) => {
+        e.preventDefault()
+        const data = new FormData()
+        data.append('theme', 'Заказать звонок');
+        data.append('name', form.name);
+        data.append('phone', form.phone);
+        if (valideModal()) {
+            axios.post(`https://armon.tj/freedom/telegramForm/php/send-message-to-telegram.php`, data)
+                .then(res => {
+                    setRes(res.data)
+                })
+                .catch((errors) => {
+                    console.log('Ошибка', `${errors.message}`);
+                })
+        } else {
+            return
+        }
+    }
+
+    const valideModal = () => {
+        const err = validation.modalValidation(form)
+        if (err.error) {
+            setValidetAlert(err.message);
+            return false
+        }
+        return true
+    }
+
     return (
         <div className="modal__wrapper">
             <Modal
@@ -13,16 +60,48 @@ export default function Index(props) {
             >
                 <Modal.Body>
                     <p className="text-center text__modal">ЗАКАЗАТЬ ЗВОНОК</p>
+                    {res === null ? "" : ReactHtmlParser(res)}
+                    {validetAlert === null ? "" : <p className="alert__paginate">{validetAlert}</p>}
                     <div className="under__modal-text d-flex justify-content-center">
                         <div className="under__text"></div>
                     </div>
                     <p className="text-center text__modal">Оставьте свои контакты и наши консультанты свяжутся с вами</p>
-                    <Form>
+                    <Form action="#" onSubmit={submitForm}>
+                        {/* <Form.Group controlId="validationCustom01">
+                            <Form.Control
+                                required
+                                name="name"
+                                onChange={handleModalInput}
+                                type="text"
+                                placeholder="Ваше имя"
+                                defaultValue=""
+                            />
+                            <Form.Control.Feedback></Form.Control.Feedback>
+                        </Form.Group>
+                        <Form.Group controlId="validationCustom02">
+                            <Form.Control
+                                required
+                                name="phone"
+                                onChange={handleModalInput}
+                                type="text"
+                                placeholder="Телефон"
+                                defaultValue=""
+                            />
+                            <Form.Control.Feedback></Form.Control.Feedback>
+                        </Form.Group> */}
                         <Form.Group controlId="formBasicName">
-                            <Form.Control type="text" placeholder="Ваше имя" />
+                            <Form.Control
+                                name="name"
+                                onChange={handleModalInput}
+                                type="text"
+                                placeholder="Ваше имя" />
                         </Form.Group>
                         <Form.Group controlId="formBasicPhone">
-                            <Form.Control type="phone" placeholder="Телефон" />
+                            <Form.Control
+                                name="phone"
+                                onChange={handleModalInput}
+                                type="phone"
+                                placeholder="Телефон" />
                         </Form.Group>
                         <div className="modal__button">
                             <input class="btn btn-primary modal__btn d-flex justify-content-center" type="submit" value="Отправить" />
