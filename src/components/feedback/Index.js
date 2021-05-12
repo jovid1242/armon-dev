2583bf46b9eb51fbd46284584ab046f83f127042
jovid1242 <jovid1242@gmail.com>
2021-05-12 17:17:17 +0500

    add feddback in home page

--- src/components/feedback/Index.js
+++ src/components/feedback/Index.js
@@ -2,21 +2,32 @@ import React, { useState } from 'react'
 import Modal from '../modal/Index'
 import './Index.css'
 
+import btnsvg from '../../assets/img/t-inbtn.svg'
+
 export default function Index() {
     const [modalShow, setModalShow] = useState(false);
     return (
         <>
             <div className="feedback__wrapper">
                 <div className="container">
-                    {/* <div className="feedback-main">
-                        <div className="text__feedback">
-                            <p>Оставьте свои контакты и наши консультанты свяжутся с вами</p>
+                    <div className="row">
+                        <div className="col-lg-8 col-md-8">
+                            <div className="feedback-main">
+                                <div className="text__feedback">
+                                    <p>Затрудняетесь с выбором? Мы ответим на ваши вопросы!</p>
+                                    <span>Оставьте свои данные и наши консультанты свяжутся с вами в ближайшее время.</span>
+                                </div>
+                            </div>
                         </div>
-                        <div className="feedback__button">
-                            <input className="btn btn-primary feedback__btn" onClick={() => setModalShow(true)} type="submit" value="Оставить заявок" />
+                        <div className="col-lg-4 col-md-4">
+                            <div className="feedback__btn">
+                                <button onClick={() => setModalShow(true)}>
+                                    <img src={btnsvg} alt="t-inbtn" />
+                                        получить консультацию
+                                    </button>
+                            </div>
                         </div>
-                    </div> */}
-
+                    </div>
                 </div>
             </div>
             <Modal
