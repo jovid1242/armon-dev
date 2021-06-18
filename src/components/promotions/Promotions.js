eaff8efcfd35fa62ba0f85ead626fbd27a178551
jovid1242 <jovid1242@gmail.com>
2021-06-18 19:08:10 +0500

    fix modal

--- src/components/promotions/Promotions.js
+++ src/components/promotions/Promotions.js
@@ -60,8 +60,52 @@ export default function Promotions() {
         <>
             <div className="promotions-section">
                 <div className="promo__modal">
-                    <Rodal visible={openImgModal.visible} onClose={hide.bind()}>
-                        sdc
+                    <Rodal visible={openImgModal.visible} onClose={hide.bind()} width="600">
+                        <div className="modal-icon__promotions">
+                            <img src={iconPromt} alt="" />
+                        </div>
+                        <div className="stock__wrapper">
+                            <div className="stock__wrapper-title">
+                                <p>Акция "УДОБНАЯ РАССРОЧКА "30/30/30"</p>
+                            </div>
+                            <div className="pdtext">
+                                <div className="ddtt"></div>
+                            </div>
+                            <div className="stock__wrapper-text">
+                                <p>
+                                    В рамках данной акции Вы можете приобрести
+                                    желаемую недвижимость
+                                    в ЖК "Freedom Residence" в рассрочку и без лишних переплат!
+                                    Вы вносите первоначальный взнос от 30%, и затем, в течение
+                                    30 месяцев выплачиваете фиксированную сумму.
+                                    "</p>
+                            </div>
+                            <div className="modal__input__group">
+                                <form action="" >
+                                    <ul>
+                                        <li>
+                                            <input
+                                                type="text"
+                                                className="mdl__input"
+                                                placeholder="Имя..."
+                                            />
+                                        </li>
+                                        <li>
+                                            <input
+                                                type="text"
+                                                className="mdl__input mt-2"
+                                                placeholder="Тел... +992__"
+                                            />
+                                        </li>
+                                    </ul>
+                                    <div className="btn__promo">
+                                        <button >
+                                            Узнать подробнее
+                                        </button>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
                     </Rodal>
                 </div>
                 <div className="container">
