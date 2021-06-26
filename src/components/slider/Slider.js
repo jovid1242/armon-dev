4efbd1361879b78c7d4a99b8edf9d7f1fa4da855
jovid1242 <jovid1242@gmail.com>
2021-06-26 15:16:41 +0500

    add slider -v 2

--- src/components/slider/Slider.js
+++ src/components/slider/Slider.js
@@ -15,7 +15,7 @@ import iconPromt from '../../assets/icons/offer_2.svg'
 
 import { Animated } from "react-animated-css";
 
-import "./Slider.css";
+import "./Index.css";
 
 export default function SliderWeb() {
     const [modalShow, setModalShow] = useState(false);
@@ -29,7 +29,6 @@ export default function SliderWeb() {
     const [openImgModal, setOpenImgModal] = useState(
         { visible: false }
     )
-
 
     const show = () => {
         setOpenImgModal({ visible: true });
@@ -67,7 +66,6 @@ export default function SliderWeb() {
             return
         }
     }
-    // jovid1242jivO
 
     const valideModal = () => {
         const err = validation.modalValidation(form)
@@ -142,76 +140,73 @@ export default function SliderWeb() {
                 </Rodal>
             </div>
             <Carousel pause={false} fade style={{ animationDelay: "2.5s" }}>
-                <Carousel.Item interval={500000} pause={false}>
-                    <img
-                        className="d-block w-100"
+                <Carousel.Item interval={5000} pause={false} style={{ background: `url(${slide3})` }}>
+                    {/* <img
+                        className="carousel__img d-block"
                         src={slide3}
                         alt="Second slide"
-                    />
+                    /> */}
                     <Carousel.Caption>
-                        <Animated animationIn="slideInRight" animationOut="fadeOut" isVisible={true}>
-                            <h3 style={{ animationDelay: "3s" }}>Акция "УДОБНАЯ <br /><br />РАССРОЧКА "30/30/30""</h3>
-                            <p style={{ animationDelay: "3s" }}>
-                                В рамках данной акции Вы можете приобрести желаемую недвижимость в ЖК "Freedom Residence" в рассрочку и без лишних переплат! Вы вносите первоначальный взнос от 30%,
-                                и затем, в течение 30 месяцев выплачиваете фиксированную сумму.
-                            </p>
-                        </Animated>
-                        <div className="slide__btn d-flex" style={{ animationDelay: "3s" }}>
-                            <button> <a href="#stackk">Узнать подробнее</a></button>
-                            <button onClick={show.bind()}>Оставить заявку</button>
-                        </div>
-                        <div className="mob__btn-slider">
-                            <button onClick={show.bind()}>Оставить заявку</button>
+                        <div className="my__auto ">
+                            <Animated animationIn="slideInRight" animationOut="fadeOut" isVisible={true}>
+                                <h3 style={{ animationDelay: "3s" }}>Акция "УДОБНАЯ<br />РАССРОЧКА "30/30/30""</h3>
+                                <p style={{ animationDelay: "3s" }}>
+                                    В рамках данной акции Вы можете приобрести желаемую недвижимость в ЖК "Freedom Residence" в рассрочку и без лишних переплат! Вы вносите первоначальный взнос от 30%,
+                                    и затем, в течение 30 месяцев выплачиваете фиксированную сумму.
+                                </p>
+                            </Animated>
+                            <div className="slide__btn d-flex" style={{ animationDelay: "3s" }}>
+                                <button> <a href="#stackk">Узнать подробнее</a></button>
+                                <button onClick={show.bind()}>Оставить заявку</button>
+                            </div>
                         </div>
                     </Carousel.Caption>
                 </Carousel.Item>
-                <Carousel.Item interval={5000} pause={false}>
-                    <img
-                        className="d-block w-100"
+                <Carousel.Item interval={5000} pause={false} style={{ background: `url(${slideww})` }}>
+                    {/* <img
+                        className="d-block carousel__img"
                         src={slideww}
                         alt="First slide"
-                    />
+                    /> */}
                     <Carousel.Caption>
-                        <Animated animationIn="slideInRight" animationOut="fadeOut" isVisible={true}>
-                            <h3 style={{ animationDelay: "3s" }} >Ispechak Residence</h3>
-                            <p style={{ animationDelay: "3s" }}>Рады представить Вам первый в Душанбе
-                                продуманный архитектурный проект в
-                                котором сочетаются красота и качество,
-                                роскошь и уют, простота и обдуманность в деталях.</p>
-                        </Animated>
-                        <div className="slide__btn d-flex" style={{ animationDelay: "3s" }}>
-                            <button>
-                                <a href="https://arc.tj/project/ispechak-residence/">
-                                    Узнать подробнее
-                                </a>
-                            </button>
-                            <button onClick={() => setModalShow(true)}>Оставить заявку</button>
-                        </div>
-                        <div className="mob__btn-slider">
-                            <button onClick={() => setModalShow(true)}>Оставить заявку</button>
+                        <div className="my__auto">
+                            <Animated animationIn="slideInRight" animationOut="fadeOut" isVisible={true}>
+                                <h3 style={{ animationDelay: "3s" }} >Ispechak Residence</h3>
+                                <p style={{ animationDelay: "3s" }}>Рады представить Вам первый в Душанбе
+                                    продуманный архитектурный проект в
+                                    котором сочетаются красота и качество,
+                                    роскошь и уют, простота и обдуманность в деталях.</p>
+                            </Animated>
+                            <div className="slide__btn d-flex" style={{ animationDelay: "3s" }}>
+                                <button>
+                                    <a href="https://arc.tj/project/ispechak-residence/">
+                                        Узнать подробнее
+                                    </a>
+                                </button>
+                                <button onClick={() => setModalShow(true)}>Оставить заявку</button>
+                            </div>
                         </div>
                     </Carousel.Caption>
                 </Carousel.Item>
-                <Carousel.Item interval={5000} pause={false}>
-                    <img
-                        className="d-block w-100"
+                <Carousel.Item interval={5000} pause={false} style={{ background: `url(${slidew})` }}>
+                    {/* <img
+                        className="d-block carousel__img"
                         src={slidew}
                         alt="Second slide"
-                    />
+                    /> */}
                     <Carousel.Caption>
-                        <Animated animationIn="slideInRight" animationOut="fadeOut" isVisible={true}>
-                            <h3 style={{ animationDelay: "3s" }}>Freedom Residence</h3>
-                            <p style={{ animationDelay: "3s" }}>
-                                Freedom Residence - место, где красота и удобство, качество и уют,
-                                безопасность и беззаботность никогда не будут спорить между собой.
-                            </p>
-                        </Animated>
-                        <div className="slide__btn d-flex" style={{ animationDelay: "3s" }}>
-                            <button> <a href="https://armon.tj/freedom">Узнать подробнее</a></button>
-                            <button onClick={() => setModalShow(true)}>Оставить заявку</button>
-                        </div>
-                        <div className="mob__btn-slider">
-                            <button onClick={() => setModalShow(true)}>Оставить заявку</button>
+                        <div className="my__auto">
+                            <Animated animationIn="slideInRight" animationOut="fadeOut" isVisible={true}>
+                                <h3 style={{ animationDelay: "3s" }}>Freedom Residence</h3>
+                                <p style={{ animationDelay: "3s" }}>
+                                    Freedom Residence - место, где красота и удобство, качество и уют,
+                                    безопасность и беззаботность никогда не будут спорить между собой.
+                                </p>
+                            </Animated>
+                            <div className="slide__btn d-flex" style={{ animationDelay: "3s" }}>
+                                <button> <a href="https://armon.tj/freedom">Узнать подробнее</a></button>
+                                <button onClick={() => setModalShow(true)}>Оставить заявку</button>
+                            </div>
                         </div>
                     </Carousel.Caption>
                 </Carousel.Item>
